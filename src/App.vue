759d19bcaeeb7babd22965218ffe6b94c146cc9a
Vladimir Rodkin <mail@vovanr.com>
2016-02-03 17:57:27 +0300

    Add Jade template engine; Add Yaml data type

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="app">
-    <textarea
-      v-model="json"
-      v-on:blur="render"
-      placeholder="JSON"
-      ></textarea>
-    <textarea
-      v-model="template"
-      v-on:blur="render"
-      placeholder="Template"
-      ></textarea>
-    <textarea
-      v-model="result"
-      placeholder="Result"
-      readonly
-      ></textarea>
+    <div class="section">
+      <select v-model="dataType" tabindex="1">
+        <option v-for="item in dataTypes" v-bind:value="item.value">
+          {{ item.name }}
+        </option>
+      </select>
+      <textarea
+        v-model="data"
+        v-on:blur="render"
+        placeholder="Data"
+        tabindex="0"
+        ></textarea>
+    </div>
+
+    <div class="section">
+      <select v-model="templateEngine" tabindex="1">
+        <option v-for="item in templateEngines" v-bind:value="item.value">
+          {{ item.name }}
+        </option>
+      </select>
+      <textarea
+        v-model="template"
+        v-on:blur="render"
+        placeholder="Template"
+        tabindex="0"
+        ></textarea>
+    </div>
+
+    <div class="section">
+      <textarea
+        v-model="result"
+        placeholder="Result"
+        readonly
+        tabindex="0"
+        ></textarea>
+    </div>
     <Copyright></Copyright>
   </div>
 </template>
@@ -22,33 +43,73 @@
 <script>
 import Copyright from './components/Copyright'
 
+const parsers = {
+  json: function (data) {
+    try {
+      return JSON.parse(data)
+    } catch (e) {
+      return ''
+    }
+  },
+  yaml: function (data) {
+    return window.jsyaml.load(data)
+  }
+}
+
+const compillers = {
+  handlebars: function (template, data) {
+    return window.Handlebars.compile(template)({items: data})
+  },
+  jade: function (template, data) {
+    return window.jade.render(template, {items: data})
+  }
+}
+
+let datas = {
+  json: `[
+  {"name": "Foo"},
+  {"name": "Bar"}
+]`,
+  yaml: `- name: Foo
+- name: Bar`
+}
+
+let templates = {
+  handlebars: `<ul>
+  {{#each items}}
+  <li>{{name}}</li>
+  {{/each}}
+</ul>`,
+  jade: `ul
+  each item in items
+    li= item.name`
+}
+
 export default {
   components: {
     Copyright
   },
   data () {
     return {
-      json: `[
-  {"name": "Foo"},
-  {"name": "Bar"}
-]`,
-      template: `<ul>
-  {{#each items}}
-  <li>{{name}}</li>
-  {{/each}}
-</ul>`,
-      result: ''
+      data: datas.json,
+      template: templates.jade,
+      result: '',
+      dataType: 'json',
+      dataTypes: [
+        {name: 'JSON', value: 'json'},
+        {name: 'Yaml', value: 'yaml'}
+      ],
+      templateEngine: 'jade',
+      templateEngines: [
+        {name: 'Handlebars', value: 'handlebars'},
+        {name: 'Jade', value: 'jade'}
+      ]
     }
   },
   methods: {
     render: function () {
-      let json
-      try {
-        json = JSON.parse(this.json)
-      } catch (e) {
-        return ''
-      }
-      this.result = window.Handlebars.compile(this.template)({items: json})
+      let data = parsers[this.dataType](this.data)
+      this.result = compillers[this.templateEngine](this.template, data)
     }
   }
 }
@@ -59,6 +120,7 @@ body {
   font-family: Helvetica, sans-serif;
   margin: 0;
 }
+
 #app {
   display: flex;
   justify-content: center;
@@ -66,20 +128,35 @@ body {
   height: 100%;
   position: absolute;
 }
+
+.section {
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+
+select {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+}
+
 textarea {
   width: 100%;
   height: 100%;
   box-sizing: border-box;
   padding: 1%;
   resize: none;
-  flex: 1;
   border: 2px solid #fff;
   transition: border ease .15s;
   outline: none;
 }
+
 textarea:focus {
   border-color: #ff3304;
 }
+
 textarea[readonly] {
   background-color: #fbfbfb;
 }
